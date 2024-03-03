b4f43885090864ae621d61ff3b8f3b47c889e413
Anjasfedo <fedoaf98@gmail.com>
2024-03-03 16:43:58 +0700

    Use Java Build-in Stack

--- src/com/algorithms/stack/ReverseStringStack.java
+++ src/com/algorithms/stack/ReverseStringStack.java
@@ -1,78 +1,11 @@
 package com.algorithms.stack;
 
-import java.util.EmptyStackException;
+import java.util.Stack;
 
 public class ReverseStringStack {
-    private ListNode top;
-
-    private int length;
-
-    private class ListNode {
-        private char data;
-        private ListNode next;
-
-        public ListNode(char data) {
-            this.data = data;
-        }
-    }
-
-    public ReverseStringStack() {
-        top = null;
-        length = 0;
-    }
-
-    public int stacklength() {
-        return length;
-    }
-
-    public boolean isEmpty() {
-        return length == 0;
-    }
-
-    public void push(char data) {
-        ListNode newNode = new ListNode(data);
-
-        newNode.next = top;
-        top = newNode;
-
-        length++;
-    }
-
-    public ListNode pop() {
-        if (isEmpty()) {
-            throw new EmptyStackException();
-        }
-
-        ListNode temp = top;
-
-        top = top.next;
-
-        length--;
-
-        return temp;
-    }
-
-    public int peekTop() {
-        if (isEmpty()) {
-            throw new EmptyStackException();
-        }
-
-        return top.data;
-    }
-
-    public void printElements() {
-        ListNode temp = top;
-
-        while (temp != null) {
-            System.out.println(temp.data);
-            temp = temp.next;
-        }
-
-        System.out.println();
-    }
 
     public static String reverseString(String str) {
-        ReverseStringStack stack = new ReverseStringStack();
+        Stack<Character> stack = new Stack<>();
 
         char[] chars = str.toCharArray();
 
@@ -81,8 +14,7 @@ public class ReverseStringStack {
         }
 
         for (int i = 0; i < str.length(); i++) {
-            char data = stack.pop().data;
-            chars[i] = data;
+            chars[i] = stack.pop();
         }
 
         return new String(chars);

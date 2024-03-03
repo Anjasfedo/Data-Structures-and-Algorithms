3a5d014e4cabbaf3e3f3cdf20e6da0a3df997f31
Anjasfedo <fedoaf98@gmail.com>
2024-03-03 17:51:05 +0700

    Reversed nextGreater

--- src/com/algorithms/stack/ReverseStringStack.java
+++ src/com/algorithms/stack/ReverseStringStack.java
@@ -1,5 +1,6 @@
 package com.algorithms.stack;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 public class ReverseStringStack {
@@ -20,13 +21,53 @@ public class ReverseStringStack {
         return new String(chars);
     }
 
+    public static int[] nextGreater(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+
+        int check = 0;
+
+        while (check < arr.length) {
+            // System.out.println(check);
+            for (int i = check + 1; i < arr.length + 1; i++) {
+                if (i == arr.length) {
+                    stack.push(-1);
+                    break;
+                }
+
+                if (arr[i] > arr[check]) {
+                    stack.push(arr[i]);
+                    break;
+                }
+
+            }
+            check++;
+        }
+
+        int[] result = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+        result[i] = stack.pop();
+        }
+        
+        return result;
+    }
+
     public static void main(String[] args) {
-        String name = "anjas";
+        // String name = "anjas";
 
-        System.out.println(name);
+        // System.out.println(name);
 
-        name = reverseString(name);
+        // name = reverseString(name);
 
-        System.out.println(name);
+        // System.out.println(name);
+
+        int[] arr = { 4, 7, 3, 4, 8, 1 };
+
+        System.out.println(Arrays.toString(arr));
+
+        arr = nextGreater(arr);
+
+        System.out.println(Arrays.toString(arr));
+
     }
 }

c0056a3c071fe63e265fcd1fdc01f8217a438bf2
Anjasfedo <fedoaf98@gmail.com>
2024-03-03 16:05:29 +0700

    Print Stack Elements

--- src/com/algorithms/stack/list/StackList.java
+++ src/com/algorithms/stack/list/StackList.java
@@ -1,5 +1,7 @@
 package com.algorithms.stack.list;
 
+import java.util.EmptyStackException;
+
 public class StackList {
 
     private ListNode top;
@@ -19,28 +21,67 @@ public class StackList {
         length = 0;
     }
 
+    public int stacklength() {
+        return length;
+    }
+
+    public boolean isEmpty() {
+        return length == 0;
+    }
+
     public void push(int data) {
         ListNode newNode = new ListNode(data);
 
-        top.next = newNode;
+        newNode.next = top;
         top = newNode;
 
         length++;
     }
 
     public ListNode pop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+
         ListNode temp = top;
 
         top = top.next;
 
         length--;
-        
+
         return temp;
+    }
+
+    public int peekTop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+
+        return top.data;
+    }
+
+    public void printElements() {
+        ListNode temp = top;
+
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+
+        System.out.println();
     }
 
     public static void main(String[] args) {
         StackList sl = new StackList();
 
-        
+        sl.push(5);
+        sl.push(5);
+        sl.push(2);
+        sl.pop();
+        sl.push(4);
+        sl.push(7);
+        sl.pop();
+
+        sl.printElements();
     }
 }

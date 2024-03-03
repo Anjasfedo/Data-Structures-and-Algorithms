efefbc461aaa6c14a6cd9b8d1b8a588486182e17
Anjasfedo <fedoaf98@gmail.com>
2024-03-03 15:52:39 +0700

    Pop and Push the Stack

--- src/com/algorithms/stack/list/StackList.java
+++ src/com/algorithms/stack/list/StackList.java
@@ -4,7 +4,7 @@ public class StackList {
 
     private ListNode top;
     private int length;
-    
+
     private class ListNode {
         private int data;
         private ListNode next;
@@ -19,7 +19,28 @@ public class StackList {
         length = 0;
     }
 
+    public void push(int data) {
+        ListNode newNode = new ListNode(data);
+
+        top.next = newNode;
+        top = newNode;
+
+        length++;
+    }
+
+    public ListNode pop() {
+        ListNode temp = top;
+
+        top = top.next;
+
+        length--;
+        
+        return temp;
+    }
+
     public static void main(String[] args) {
+        StackList sl = new StackList();
+
         
     }
 }

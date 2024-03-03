6bdba32115d0b15d07f38eb403a789252d5138ce
Anjasfedo <fedoaf98@gmail.com>
2024-03-03 16:20:21 +0700

    Push Elements on Array Stack

--- src/com/algorithms/stack/array/StackArray.java
+++ src/com/algorithms/stack/array/StackArray.java
@@ -11,6 +11,27 @@ public class StackArray {
         arr = new int[capacity];
     }
 
+    public int size() {
+        return top + 1;
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public boolean isFull() {
+        return size() == arr.length;
+    }
+
+    public void push(int data) {
+        if (isFull()) {
+            throw new RuntimeException("Stack full");
+        }
+
+        top++;
+        arr[top] = data;
+    }
+
     public static void main(String[] args) {
         
     }

9e81997ee1055f786293214004a07d293290d0f6
Anjasfedo <fedoaf98@gmail.com>
2024-03-04 22:37:19 +0700

    generateBinaryNumbers Function

--- src/com/algorithms/queue/Queue.java
+++ src/com/algorithms/queue/Queue.java
@@ -1,5 +1,6 @@
 package com.algorithms.queue;
 
+import java.util.LinkedList;
 import java.util.NoSuchElementException;
 
 public class Queue {
@@ -115,6 +116,26 @@ public class Queue {
         // queue.printElements();
     }
 
+    public static String[] generateBinaryNumbers(int n) {
+        String[] result = new String[n];
+
+        java.util.Queue<String> q = new LinkedList<>();
+
+        q.offer("1");
+        
+        for (int i = 0; i < n; i++) {
+            result[i] = q.poll();
+
+            String n1 = result[i] + "0";
+            String n2 = result[i] + "1";
+
+            q.offer(n1);
+            q.offer(n2);
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
         // Queue queue = new Queue();
 
@@ -127,6 +148,11 @@ public class Queue {
 
         // queue.printElements();
 
-        generateBinaryNum(5);
+        String[] binaryNumbers = generateBinaryNumbers(5);
+
+        for (String string : binaryNumbers) {
+            System.out.println(string);
+        }
+        
     }
 }

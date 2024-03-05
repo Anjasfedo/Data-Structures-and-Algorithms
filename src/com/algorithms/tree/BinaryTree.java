afb7128eb293e2fa8967b89a77e286f2d6a3644f
Anjasfedo <fedoaf98@gmail.com>
2024-03-05 15:05:04 +0700

    Iterative Pre-Order Traversal of Binary Tree

--- src/com/algorithms/tree/BinaryTree.java
+++ src/com/algorithms/tree/BinaryTree.java
@@ -1,5 +1,7 @@
 package com.algorithms.tree;
 
+import java.util.Stack;
+
 public class BinaryTree {
 
     private TreeNode root;
@@ -16,19 +18,18 @@ public class BinaryTree {
         }
     }
 
-    public TreeNode createBinaryTree() {
+    public void createBinaryTree() {
         TreeNode first = new TreeNode(1);
         TreeNode second = new TreeNode(2);
         TreeNode third = new TreeNode(3);
         TreeNode fourth = new TreeNode(4);
         TreeNode fifth = new TreeNode(5);
 
+        root = first;
         first.left = second;
         first.right = third;
         second.left = fourth;
         second.right = fifth;
-
-        return first;
     }
 
     public void preOrder(TreeNode root) {
@@ -41,11 +42,39 @@ public class BinaryTree {
         preOrder(root.right);
     }
 
+    public void iterativePreOrder() {
+        if (root == null) {
+            return;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode temp = stack.pop();
+
+            System.out.print(temp.data + " ");
+
+            if (temp.right != null) {
+                stack.push(temp.right);
+            }
+
+            if (temp.left != null) {
+                stack.push(temp.left); 
+            }
+        }
+    }
+
     public static void main(String[] args) {
         BinaryTree bt = new BinaryTree();
 
-        bt.root = bt.createBinaryTree();
+        bt.createBinaryTree();
 
         bt.preOrder(bt.root);
+
+        System.out.println();
+
+        bt.iterativePreOrder();
     }
 }

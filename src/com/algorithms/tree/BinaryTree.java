e93dafc57fe99606858ebc3778bc69ef45982eb7
Anjasfedo <fedoaf98@gmail.com>
2024-03-05 14:39:31 +0700

    Initalize Binary Tree

---
+++ src/com/algorithms/tree/BinaryTree.java
@@ -0,0 +1,22 @@
+package com.algorithms.tree;
+
+public class BinaryTree {
+
+    private TreeNode root;
+    
+    public class TreeNode {
+        private int data;
+        private TreeNode left;
+        private TreeNode right;
+
+        public TreeNode(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}

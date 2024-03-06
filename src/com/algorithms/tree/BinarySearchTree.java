2243c8e7fb4c4fe831a2fdc6b333c9a895aceff2
AnjasFedo <fedoaf98@gmail.com>
2024-03-06 21:36:31 +0700

    Recursive Insert Node on Binary Search Tree

--- src/com/algorithms/tree/BinarySearchTree.java
+++ src/com/algorithms/tree/BinarySearchTree.java
@@ -15,7 +15,43 @@ public class BinarySearchTree {
         }
     }
 
+    public void insert(int data) {
+        root = insert(root, data);
+    }
+
+    public TreeNode insert(TreeNode root, int data) {
+        if (root == null) {
+            root = new TreeNode(data);
+            return root;
+        }
+
+        if (data < root.data) {
+            root.left = insert(root.left, data);
+        } else {
+            root.right = insert(root.right, data);
+        }
+
+        return root;
+    }
+
+    public void inOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+    }
+
     public static void main(String[] args) {
-        
+        BinarySearchTree bst = new BinarySearchTree();
+
+        bst.insert(5);
+        bst.insert(3);
+        bst.insert(8);
+        bst.insert(2);
+
+        bst.inOrder(bst.root);
     }
 }

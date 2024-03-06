cacc541cd1a428c76a84632e167fffc31d95048f
AnjasFedo <fedoaf98@gmail.com>
2024-03-06 21:25:51 +0700

    Initialize Binary Search Tree

---
+++ src/com/algorithms/tree/BinarySearchTree.java
@@ -0,0 +1,21 @@
+package com.algorithms.tree;
+
+public class BinarySearchTree {
+    private TreeNode root;
+
+    public class TreeNode {
+        private int data;
+        private TreeNode left;
+        private TreeNode right;
+
+        public TreeNode(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
